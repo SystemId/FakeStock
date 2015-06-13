83151bc06f820fcd7f059329912bc91032cc4b87
SystemId <anivon4@gmail.com>
2015-06-12 22:47:34 -0400

    hjh

---
+++ datasvc/src/test/java/Datasvc/datasvc/TestMongoDB.java
@@ -0,0 +1,11 @@
+package Datasvc.datasvc;
+
+import com.ani.stock.mongodb.MongoDb;
+
+public class TestMongoDB {
+	
+	public static void main(String[] args) {
+		MongoDb mongo = new MongoDb();
+	}
+
+}
